fd25af2f610a57b887fa1634de6acf4b3d496382
SilentSkins <ayandon563@gmail.com>
2025-06-08 01:37:20 +0500

    Delete upload.php

--- upload.php
+++
@@ -1,9 +0,0 @@
-<?php
-$target_dir = "uploads/";
-$target_file = $target_dir . basename($_FILES["skinFile"]["name"]);
-if (move_uploaded_file($_FILES["skinFile"]["tmp_name"], $target_file)) {
-    echo "Upload successful.";
-} else {
-    echo "Error uploading file.";
-}
-?>
